4f7907578329e141ef808a25f35f71127eb1be3d
GangSu <woorame01@gmail.com>
2026-09-24 17:23:40 +0900

    도복 클래스 작성

---
+++ src/Equipment/Equipment_Dobok.java
@@ -0,0 +1,20 @@
+package Equipment;
+
+public class Equipment_Dobok {
+	private String number;
+	private String date;
+	
+	public Equipment_Dobok(String number, String date) {
+		this.number = number;
+		this.date = date;
+	}
+	
+	public String getNumber() {
+		return number;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+
+}
